85790d1407106c027f474f0dc30dfe3705118a12
Pepe <jm.ruizmateos@plannifai.com>
2023-02-15 03:44:12 +0000

    [NEW] Houese Photos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,12 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  hogwartsHouses : String[] = [
-    'Griffindor',
-    'Slytherin',
-    'Huffelpuf',
-    'Ravenclaw'
+  hogwartsHouses : {id: string, foto: string}[] = [
+    { id:'Griffindor',
+      foto: 'https://logos-world.net/wp-content/uploads/2021/08/Gryffindor-Logo.png'},
+      { id:'Ravenclaw',
+      foto: 'https://logos-world.net/wp-content/uploads/2022/11/Ravenclaw-Logo.png'},    
+      { id:'Slytherin',
+      foto: 'https://logos-world.net/wp-content/uploads/2022/02/Slytherin-Logo.png'},    
+      { id:'Hufflepuff',
+      foto: 'https://static.wikia.nocookie.net/pottermore/images/5/5e/Hufflepuff_crest.png'}
   ];
+
 
   constructor() { }
 
